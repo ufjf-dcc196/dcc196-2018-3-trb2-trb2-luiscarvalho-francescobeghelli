b587596ee5d16d0d64997e249b4960e10b62eb5e
Francesco <francesco_jf@hotmail.com>
2018-10-24 18:07:39 -0300

    Ajuste Activity Cadastro

--- app/src/main/java/br/ufjf/dcc196/myapplication/CadastroParticipanteActivity.java
+++ app/src/main/java/br/ufjf/dcc196/myapplication/CadastroParticipanteActivity.java
@@ -2,6 +2,7 @@ package br.ufjf.dcc196.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,5 +17,19 @@ public class CadastroParticipanteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_participante);
+
+        edtNome = (EditText)findViewById(R.id.edtNomeCompleto);
+        edtEmail = (EditText)findViewById(R.id.edtEmail);
+        edtCpf = (EditText)findViewById(R.id.edtCpf);
+        btnCadastrar = (Button)findViewById(R.id.btnCadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                finish();
+            }
+        });
     }
 }
