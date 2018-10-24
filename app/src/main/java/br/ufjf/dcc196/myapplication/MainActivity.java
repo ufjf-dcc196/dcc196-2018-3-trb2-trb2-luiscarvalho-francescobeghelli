5f352b9ba0badef9881e1bacad9422abe02aba62
FrancescoBeghelli <francesco_jf@hotmail.com>
2018-10-24 08:27:24 -0300

    Criação da activity Cadastro

--- app/src/main/java/br/ufjf/dcc196/myapplication/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/myapplication/MainActivity.java
@@ -1,13 +1,28 @@
 package br.ufjf.dcc196.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCadPart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCadPart = (Button)findViewById(R.id.btnCadParticipante);
+
+        btnCadPart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,CadastroParticipanteActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
