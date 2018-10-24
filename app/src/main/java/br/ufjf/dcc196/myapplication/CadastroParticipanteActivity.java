9a15a799e13c77b6ab5589f2ebcfc1e76c0de01a
FrancescoBeghelli <francesco_jf@hotmail.com>
2018-10-24 08:54:47 -0300

    Commit ajuste activity CadastroParticipante

--- app/src/main/java/br/ufjf/dcc196/myapplication/CadastroParticipanteActivity.java
+++ app/src/main/java/br/ufjf/dcc196/myapplication/CadastroParticipanteActivity.java
@@ -2,8 +2,15 @@ package br.ufjf.dcc196.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CadastroParticipanteActivity extends AppCompatActivity {
+
+    private EditText edtNome;
+    private EditText edtEmail;
+    private EditText edtCpf;
+    private Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
