24c348edde81a13b74a782706bedd3b1c2e970dd
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2018-11-25 20:07:20 -0200

    Cursor de eventos que participa

--- app/src/main/java/br/ufjf/dcc196/myapplication/InscricaoContract.java
+++ app/src/main/java/br/ufjf/dcc196/myapplication/InscricaoContract.java
@@ -25,4 +25,11 @@ public class InscricaoContract {
                 " i ON e." + EventoContract.Evento._ID + " = i." + Inscricao.COLUMN_NAME_ID_EVENTO + " WHERE i." + Inscricao.COLUMN_NAME_ID_PARTICIPANTE
                 + " != ?", new String[] { Integer.toString(id) });
     }
+
+    public static Cursor getEventosQueParticipaCursor(SQLiteDatabase db, int id)
+    {
+        return db.rawQuery("SELECT DISTINCT *.e from " + EventoContract.Evento.TABLE_NAME + " e LEFT JOIN " + Inscricao.TABLE_NAME +
+                " i ON e." + EventoContract.Evento._ID + " = i." + Inscricao.COLUMN_NAME_ID_EVENTO + " WHERE i." + Inscricao.COLUMN_NAME_ID_PARTICIPANTE
+                + " = ?", new String[] { Integer.toString(id) });
+    }
 }
