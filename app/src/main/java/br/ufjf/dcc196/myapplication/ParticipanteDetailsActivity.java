9878a6bd572bac9799244cc7309c61b2d92ed45d
FrancescoBeghelli <francesco_jf@hotmail.com>
2018-11-25 18:05:51 -0200

    Ajuste DetailsActivity Funcionando

--- app/src/main/java/br/ufjf/dcc196/myapplication/ParticipanteDetailsActivity.java
+++ app/src/main/java/br/ufjf/dcc196/myapplication/ParticipanteDetailsActivity.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ParticipanteDetailsActivity extends AppCompatActivity {
@@ -13,13 +14,18 @@ public class ParticipanteDetailsActivity extends AppCompatActivity {
     private TextView txtEmail;
     private TextView txtCpf;
 
-    Bundle bundle = getIntent().getExtras();
-    final String id = Integer.toString(bundle.getInt("id"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_participante_details);
+
+        txtNome = (TextView)findViewById(R.id.txtNomePartEvent);
+        txtEmail = (TextView)findViewById(R.id.txtEmailPartEvent);
+        txtCpf = (TextView)findViewById(R.id.txtCPFPartEvent);
+
+        Bundle bundle = getIntent().getExtras();
+        final String id = Integer.toString(bundle.getInt("id"));
 
         SQLiteDatabase db = new ParticipanteEventoDbHelper(this).getWritableDatabase();
 
@@ -27,11 +33,10 @@ public class ParticipanteDetailsActivity extends AppCompatActivity {
         int idxNome = cursor.getColumnIndexOrThrow(ParticipanteContract.Participante.COLUMN_NAME_NOME);
         int idxEmail = cursor.getColumnIndexOrThrow(ParticipanteContract.Participante.COLUMN_NAME_EMAIL);
         int idxCPF = cursor.getColumnIndexOrThrow(ParticipanteContract.Participante.COLUMN_NAME_CPF);
+        cursor.moveToNext();
         txtNome.setText(cursor.getString(idxNome));
         txtEmail.setText(cursor.getString(idxEmail));
         txtCpf.setText(cursor.getString(idxCPF));
 
     }
 }
-
-//cv.getAsString();
