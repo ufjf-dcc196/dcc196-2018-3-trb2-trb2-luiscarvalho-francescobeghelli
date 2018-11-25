2f8d54284844027d04e281efa39c978fcc3d6f23
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2018-11-25 19:56:49 -0200

    Cursor para eventos nao inscritos

--- app/src/main/java/br/ufjf/dcc196/myapplication/InscricaoContract.java
+++ app/src/main/java/br/ufjf/dcc196/myapplication/InscricaoContract.java
@@ -1,6 +1,9 @@
 package br.ufjf.dcc196.myapplication;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
+import android.util.EventLog;
 
 public class InscricaoContract {
     public final class Inscricao implements BaseColumns
@@ -14,5 +17,12 @@ public class InscricaoContract {
                 + InscricaoContract.Inscricao.COLUMN_NAME_ID_PARTICIPANTE + " INTEGER REFERENCES " + ParticipanteContract.Participante.TABLE_NAME + "(" + ParticipanteContract.Participante._ID + ") "
                 + ")";
         public static final String DROP_INSCRICAO = "DROP TABLE IF EXISTS " + InscricaoContract.Inscricao.TABLE_NAME;
+    }
+
+    public static Cursor getEventosQueNaoParticipaCursor(SQLiteDatabase db, int id)
+    {
+        return db.rawQuery("SELECT DISTINCT *.e from " + EventoContract.Evento.TABLE_NAME + " e LEFT JOIN " + Inscricao.TABLE_NAME +
+                " i ON e." + EventoContract.Evento._ID + " = i." + Inscricao.COLUMN_NAME_ID_EVENTO + " WHERE i." + Inscricao.COLUMN_NAME_ID_PARTICIPANTE
+                + " != ?", new String[] { Integer.toString(id) });
     }
 }
